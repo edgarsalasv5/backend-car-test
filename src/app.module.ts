12ec85e4455f199584d0727a59db99ec5bac4909
edgarsalasv5 <edgarsalasv5@gmail.com>
2022-06-20 19:15:25 -0500

    Add: .env to database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { validationConfigSchema } from './config';
+import { getConfig, validationConfigSchema } from './config';
 import { AuthModule } from './modules/auth/auth.module';
 import { CarModule } from './modules/car/car.module';
 import { UserModule } from './modules/user/user.module';
@@ -16,10 +16,10 @@ import { UserEntity } from './modules/user/entities/user.entity';
     TypeOrmModule.forRoot({
       port: 5432,
       type: 'postgres',
-      host: 'db-test.cjobv6edlmdo.us-east-2.rds.amazonaws.com',
-      username: 'postgres',
-      password: 'passwordpulpo',
-      database: 'postgres',
+      host: getConfig().DB_HOST,
+      username: getConfig().DB_USER,
+      password: getConfig().DB_PASS,
+      database: getConfig().DB_NAME,
       synchronize: true,
       entities: [UserEntity],
     }),
